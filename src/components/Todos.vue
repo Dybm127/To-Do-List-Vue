68eccb79a2ebc795adef6477cc1c41ed3fa3ede3
Derian <derian12702@gmail.com>
2021-08-23 15:09:04 +0200

    archivos completos

---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<div v-for="todo in todosList"  v-bind:key="todo.id">
+			<TodoItem v-bind:todo="todo" v-on:delete-todo="$emit('delete-todo', todo.id)" />
+		</div>
+	</div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+	export default {
+		name: 'Todos',
+		props: ['todosList'], 
+		components: {TodoItem}
+	}
+	
+</script>
+
+<style>
+</style>
